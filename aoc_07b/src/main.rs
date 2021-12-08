ab9fe5609454f92928a39be4eff20cd2c0bcd602
Jesús Torres <jmtorres@ull.es>
2021-12-08 01:59:55 +0000

    Day 7

--- aoc_07b/src/main.rs
+++ aoc_07b/src/main.rs
@@ -1,55 +1,16 @@
-fn triangle_number(number: f32) -> f32 {
+use ordered_float::OrderedFloat;
+
+fn triangle_number(number: f64) -> f64 {
     (number * (number + 1.0)) / 2.0
 }
 
-fn average(numbers: &[f32]) -> f32 {
-    let numerator = numbers.iter().fold(0.0, |acc, n| acc + n);
-    numerator / (numbers.len() as f32)
+fn average(positions: &[u32]) -> f64 {
+    let f32_positions: Vec<f64> = positions.iter().map(|x| *x as f64).collect();
+    f32_positions.iter().fold(0.0, |acc, x| acc + x) / positions.len() as f64
 }
 
-fn fuel_wasted(positions: &[f32], final_place: f32) -> f32 {
-    positions.iter().fold(0.0, |acc, pos| acc + triangle_number((pos - final_place).abs()))
-}
-
-fn calculate_best(positions: &[f32]) -> f32 {
-    let num_of_positions = positions.len() as f32;
-    let median = positions[positions.len() / 2];
-    let mean = average(&positions);
-
-    let mut current_place: f32 = median;
-    let mut prev_place: f32 = 0.0;
-    let mut prev_epsilon: f32 = 0.0;
-    
-    loop {
-        let num_of_greater = positions.iter().filter(|&value| *value > current_place).count() as f32;
-        let num_of_less = positions.iter().filter(|&value| *value < current_place).count() as f32;
-
-        let current_epsilon = current_place - mean + (num_of_less - num_of_greater) / (num_of_positions * 2.0);
-        println!("{:?}", current_place);
-        println!("{:?}", current_epsilon);
-
-        if current_epsilon < 0.0 {
-            if prev_epsilon > 0.0 {
-                return if current_epsilon.abs() > prev_epsilon { prev_place } else { current_place };
-            }
-
-            prev_place = current_place;
-            current_place += 1.0;
-            prev_epsilon = current_epsilon;
-        }
-        else if current_epsilon > 0.0 {
-            if prev_epsilon < 0.0 {
-                return if current_epsilon > prev_epsilon.abs() { prev_place } else { current_place };
-            }
-
-            prev_place = current_place;
-            current_place -= 1.0;
-            prev_epsilon = current_epsilon;
-        }
-        else  {
-            return current_place;
-        }
-    }
+fn fuel_wasted(positions: &[u32], place: f64) -> f64 {
+    positions.iter().fold(0.0, |acc, pos| acc + triangle_number((*pos as f64 - place).abs()))
 }
 
 fn prepare_aoc_07b_input(filename: &str) -> Result<Vec<u32>, std::io::Error> {
@@ -57,12 +18,19 @@ fn prepare_aoc_07b_input(filename: &str) -> Result<Vec<u32>, std::io::Error> {
     Ok(content.split(",").map(|value| value.parse::<u32>().unwrap()).collect())
 }
 
-fn solve_aoc_07b(input: &[u32]) -> f32 {
+fn solve_aoc_07b(input: &[u32]) -> f64 {
     //println!("{:?}", input);
-    let mut positions: Vec<f32> = input.iter().map(|value| *value as f32).collect();
-    positions.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    let final_place = calculate_best(&positions);
-    fuel_wasted(&positions, final_place)
+    let mut positions = input.to_vec();
+    positions.sort_unstable();
+    let min = positions[0];
+    let max = positions[positions.len() - 1];
+
+    let best = (min..=max).map(|pos| (pos, fuel_wasted(&positions, pos as f64)))
+        .min_by_key(|value| OrderedFloat(value.1)).unwrap();
+
+    println!("{:?}", average(&positions));
+
+    best.1
 }
 
 fn main() -> Result<(), std::io::Error> {
