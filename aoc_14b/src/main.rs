6a696c655b5ed2ef424118076295bfe7d7a99f63
Jesús Torres <jmtorres@ull.es>
2021-12-15 11:09:43 +0000

    Day 14: Part 2 is too slow yet

--- aoc_14b/src/main.rs
+++ aoc_14b/src/main.rs
@@ -1,37 +1,38 @@
 use regex::Regex;
-use std::collections::HashMap;
 
-fn prepare_aoc_14b_input(filename: &str) -> Result<(Vec<u8>, HashMap<[u8; 2], u8>), std::io::Error> {
+fn prepare_aoc_14b_input(filename: &str) -> Result<(Vec<u8>, Vec<u8>), std::io::Error> {
     let content = std::fs::read_to_string(filename)?;
     let mut lines = content.lines();
 
-    let sequence = lines.next().unwrap().bytes().collect();
+    let sequence = lines.next().unwrap().bytes().map(|c| c - 65).collect();
     lines.next();
 
     let line_regex = Regex::new(r"(..) -> (.)").unwrap();
-    let mut rules: HashMap<[u8; 2], u8> = HashMap::new();
+    let mut rules: Vec<u8> = vec![255; 26 * 26];
     for line in lines.filter(|line| !line.is_empty()) {
         let captures = line_regex.captures(line).unwrap();
         let pair: [u8; 2] = captures.get(1).unwrap().as_str().as_bytes().try_into().unwrap();
-        let element = captures.get(2).unwrap().as_str().as_bytes()[0];
-        rules.insert(pair, element);
+        let element = captures.get(2).unwrap().as_str().as_bytes()[0] - 65;
+        rules[(pair[0] - 65) as usize * 26 + (pair[1] - 65) as usize] = element;
     }
 
     Ok((sequence, rules))
 }
 
-fn update_histogram(pair: &[u8; 2], rules: &HashMap<[u8; 2], u8>, histogram: &mut [usize; 128], step: usize) {
-    if step > 0  && rules.contains_key(pair) {
-            histogram[rules[pair] as usize] += 1;
-            update_histogram(&[pair[0], rules[pair]], &rules, histogram, step - 1);
-            update_histogram(&[rules[pair], pair[1]], &rules, histogram, step - 1);
+fn update_histogram(pair: &[u8; 2], rules: &[u8], histogram: &mut [usize; 26], step: usize) {
+    let rules_index = pair[0] as usize * 26 + pair[1] as usize;
+    let new_element = rules[rules_index];
+    if step > 0  && new_element != 255 {
+            histogram[new_element as usize] += 1;
+            update_histogram(&[pair[0], new_element], &rules, histogram, step - 1);
+            update_histogram(&[new_element, pair[1]], &rules, histogram, step - 1);
     }
 }
 
-fn solve_aoc_14b(sequence: &[u8], rules: &HashMap<[u8; 2], u8>) -> usize {
+fn solve_aoc_14b(sequence: &[u8], rules: &[u8]) -> usize {
     //println!("{:?}\n{:?}", sequence, rules);
 
-    let mut histogram: [usize; 128] = [0; 128];
+    let mut histogram: [usize; 26] = [0; 26];
     
     histogram[sequence[0] as usize] += 1;
     for i in 0..sequence.len() - 1 {
